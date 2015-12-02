2114b69d746f2cd538acf02cdcb919ee10355bc6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-02 01:54:06 -0500

    added fade transition

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 var Vue = require('vue')
 
+Vue.transition('fade', {
+  css: false,
+  enter: function (el, done) {
+    // element is already inserted into the DOM
+    // call done when animation finishes.
+    $(el)
+      .css('opacity', 0)
+      .animate({ opacity: 1 }, 200, done)
+  },
+  enterCancelled: function (el) {
+    $(el).stop()
+  },
+  leave: function (el, done) {
+    // same as enter
+    $(el).animate({ opacity: 0 }, 200, done)
+  },
+  leaveCancelled: function (el) {
+    $(el).stop()
+  }
+})
+
 new Vue({
   el: '#projects-list',
   data: {
